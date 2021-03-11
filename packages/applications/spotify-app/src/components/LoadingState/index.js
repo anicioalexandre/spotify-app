e76618399f250bef8390d03d26afe2bd72664758
alexandre.anicio <alexandre.anicio@smarttbotter.com>
2021-03-11 15:53:25 -0300

    feat: creates LoadingState component

---
+++ packages/applications/spotify-app/src/components/LoadingState/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+const LoadingState = () => {
+  return (
+    <div className="card">
+      <div className="image" />
+      <div className="content">
+        <h2></h2>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingState
