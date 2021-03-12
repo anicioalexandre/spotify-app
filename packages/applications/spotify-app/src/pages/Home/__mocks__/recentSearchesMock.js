03a52b88999f76973a4feae0ffe56a59ccf8a603
alexandre.anicio <alexandre.anicio@smarttbotter.com>
2021-03-12 16:49:19 -0300

    feat: creates Home page component

---
+++ packages/applications/spotify-app/src/pages/Home/__mocks__/recentSearchesMock.js
@@ -0,0 +1,25 @@
+const recentSearchesMock = {
+  last_results: [
+    {
+      trackId: '3BQHpFgAp4l80e1XslIjNI',
+      albumId: '0PT5m6hwPRrpBwIHVnvbFX',
+      image: 'https://i.scdn.co/image/ab67616d00001e02e3e3b64cea45265469d4cafa',
+      artistName: 'The Beatles',
+      name: 'Yesterday - Remastered 2009'
+    },
+    {
+      albumId: '1vANZV20H5B4Fk6yf7Ot9a',
+      image: 'https://i.scdn.co/image/ab67616d00001e0255612ece447bec5d62c68375',
+      name: 'Beatles For Sale (Remastered)',
+      artistName: 'The Beatles'
+    },
+    {
+      albumId: '2zeWAYgh0WfMhiHwvUV15b',
+      image: 'https://i.scdn.co/image/ab67616d00001e029f08c2a93ef090a1e1e6bf0c',
+      name: 'Nó na Orelha',
+      artistName: 'Criolo'
+    }
+  ]
+}
+
+module.exports = { recentSearchesMock }
--- packages/applications/spotify-app/src/pages/Home/index.js
+++ packages/applications/spotify-app/src/pages/Home/index.js
@@ -1,5 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import 'ds-image-label'
+import 'ds-fieldset'
+import 'ds-button'
 
-const Home = () => <div>Home Page</div>
+import './style.css'
+import { LABELS } from './constants'
+import SearchInput from '../../components/SearchInput'
+import { clearLocalStorage, getLocalStorage } from '../../utils/localStorage'
+import { setClickedTrack } from '../../redux/modules/track'
+import EmptyState from '../../components/EmptyState'
 
-export default Home
+const Home = ({ setClickedTrackAction }) => {
+  const [recentSearches, setRecentSearches] = useState([])
+
+  useEffect(() => {
+    setRecentSearches(getLocalStorage())
+  }, [])
+  return (
+    <div className="home-container">
+      <SearchInput />
+      <ds-fieldset label={LABELS.recentSearch}>
+        {!recentSearches?.length ? (
+          <EmptyState customLabel={LABELS.emptyState} />
+        ) : (
+          recentSearches.map(
+            ({ albumId, trackId, image, name, artistName }) => (
+              <Link
+                onClick={() => setClickedTrackAction(trackId)}
+                key={trackId || albumId}
+                to={`/album/${albumId}`}
+              >
+                <ds-image-label
+                  data-testid="album-track-card"
+                  imageSrc={image}
+                  primaryLabel={name}
+                  secondaryLabel={artistName}
+                />
+              </Link>
+            )
+          )
+        )}
+      </ds-fieldset>
+      {!!recentSearches?.length && (
+        <ds-button
+          onClick={() => {
+            setRecentSearches([])
+            clearLocalStorage()
+          }}
+        >
+          Limpar histórico
+        </ds-button>
+      )}
+    </div>
+  )
+}
+
+const mapDispatchToProps = {
+  setClickedTrackAction: setClickedTrack
+}
+
+Home.propTypes = {
+  setClickedTrackAction: PropTypes.func
+}
+
+export default connect(null, mapDispatchToProps)(Home)
